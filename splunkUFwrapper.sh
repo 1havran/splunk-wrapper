244fac1d355f394d6d664d20752c71d29fa1c278
hvr <havran.init0@gmail.com>
2016-12-18 10:43:23 +0000

    error if ldap is not reachable added override file replacement in deploymentclient.conf

--- splunkUFwrapper.sh
+++ splunkUFwrapper.sh
@@ -5,11 +5,13 @@
 # 2. LDAP_OUTPUT_PARAM is statically extracted to separate variables as arrays in BASH are not supported by all shells
 # 3. Values from LDAP_OUTPUT_PARAM are greped against ROUTING_MAP to find defined destination
 #
-# splk_routing_map.cfg structure:
+# The version in splk_routing_map should be manually increased when doing updates to the routing map to track the issues
+# splk_routing_map.conf structure:
 # version:n
 # param1,param2,destination
 #
-# the version should be manually increased when doing updates to the routing map to track the issues
+# splk_override.conf structure (one line):
+# hostname
 
 LDAP_HOST="ldap.compute.internal"
 LDAP_HOST="localhost"
@@ -17,9 +19,14 @@ LDAP_PORT="389"
 LDAP_BASEDN="dc=eu-west-2,dc=compute,dc=internal"
 LDAP_INPUT_PARAM="(uid=wlutz)"
 LDAP_OUTPUT_PARAM="uidNumber gidNumber"
-ROUTING_MAP="splk_routing_map.cfg"
+
+ROUTING_MAP="./splk_routing_map.conf"
+OVERRIDE_FILE="./splk_override.conf"
+#DEPLOYMENTCLIENT_CONF="/opt/splunkforwarder/etc/system/default/deploymentclient.conf"
+DEPLOYMENTCLIENT_CONF="./deploymentclient.conf"
+
 USE_STDOUT=1
-#USE_LOGGER=0
+#USE_LOGGER=1
 
 logme() {
 	if [ $USE_STDOUT ]; then
@@ -48,26 +55,32 @@ getDeploymentServer() {
 	FILE=$($MKTEMP)
 	if [ $FILE ]; then
 		$LDAPSEARCH_CMD > $FILE
-		#dont use bash array as it is not supported by old shells
-		HOSTCOUNTRY=$(grep uidNumber $FILE | $CUT -d' ' -f2-)
-		INFRADOMAIN=$(grep gidNumber $FILE | $CUT -d' ' -f2-)
-		if [ $HOSTCOUNTRY ] && [ $INFRADOMAIN ]; then
-			if [ ! -f $ROUTING_MAP ]; then
-				logme "func=getDeploymentServer msg=\"cannot locate $ROUTING_MAP\" dir=$(pwd) status=ko"
-			else
-				ROUTING_MAP_VERSION=$(grep version $ROUTING_MAP 2>/dev/null)
-				$(grep -q "^$HOSTCOUNTRY,$INFRADOMAIN" $ROUTING_MAP 2>/dev/null)
-				if [ $? = 0 ]; then
-					#uniq in case there are duplicates
-					SPLK_DEST_DS=$(grep "^$HOSTCOUNTRY,$INFRADOMAIN" $ROUTING_MAP | $CUT -d, -f3 | sort | uniq | head -n 1)
-					logme "func=getDeploymentServer msg=\"DS found\" ds=$SPLK_DEST_DS HOSTCOUNTRY=$HOSTCOUNTRY INFRADOMAIN=$INFRADOMAIN splk_routing_map=\"$ROUTING_MAP_VERSION\" status=ok"
-					DS_OK=1
-				else
-					logme "func=getDeploymentServer msg=\"cannot find valid DS in $ROUTING_MAP\" HOSTCOUNTRY=$HOSTCOUNTRY INFRADOMAIN=$INFRADOMAIN splk_routing_map=\"$ROUTING_MAP_VERSION\" status=ko"
-				fi
-			fi
+		LDAP_ERROR_CODE=$?
+		if [ $LDAP_ERROR_CODE -gt 0 ]; then
+			logme "func=getDeploymentServer ldaperror=$LDAP_ERROR_CODE msg=\"cannot connect to LDAP\" ldapsearch=\"$LDAPSEARCH_CMD\" status=ko"
 		else
-			logme "func=getDeploymentServer msg=\"one of LDAP parameters missing\" HOSTCOUNTRY=$HOSTCOUNTRY INFRADOMAIN=$INFRADOMAIN status=ko"
+			#dont use bash array as it is not supported by old shells
+			HOSTCOUNTRY=$(grep uidNumber $FILE | $CUT -d' ' -f2-)
+			INFRADOMAIN=$(grep gidNumber $FILE | $CUT -d' ' -f2-)
+			if [ $HOSTCOUNTRY ] && [ $INFRADOMAIN ]; then
+				if [ ! -f $ROUTING_MAP ]; then
+					logme "func=getDeploymentServer msg=\"cannot locate $ROUTING_MAP\" dir=$(pwd) status=ko"
+				else
+					ROUTING_MAP_VERSION=$(grep version $ROUTING_MAP 2>/dev/null)
+					$(grep -q "^$HOSTCOUNTRY,$INFRADOMAIN" $ROUTING_MAP 2>/dev/null)
+					if [ $? = 0 ]; then
+						#uniq in case there are duplicates
+						SPLK_DEST_DS=$(grep "^$HOSTCOUNTRY,$INFRADOMAIN" $ROUTING_MAP | $CUT -d, -f3 | sort | uniq | head -n 1)
+
+						logme "func=getDeploymentServer msg=\"DS found\" ds=$SPLK_DEST_DS HOSTCOUNTRY=$HOSTCOUNTRY INFRADOMAIN=$INFRADOMAIN splk_routing_map=\"$ROUTING_MAP_VERSION\" status=ok"
+
+					else
+						logme "func=getDeploymentServer msg=\"cannot find valid DS in $ROUTING_MAP\" HOSTCOUNTRY=$HOSTCOUNTRY INFRADOMAIN=$INFRADOMAIN splk_routing_map=\"$ROUTING_MAP_VERSION\" status=ko"
+					fi
+				fi
+			else
+				logme "func=getDeploymentServer msg=\"one of LDAP parameters missing\" HOSTCOUNTRY=$HOSTCOUNTRY INFRADOMAIN=$INFRADOMAIN status=ko"
+			fi
 		fi
 		rm $FILE 2>/dev/null
 	else
@@ -76,7 +89,14 @@ getDeploymentServer() {
 }
 
 setDeploymentServer() {
-	logme "func=setDeploymentServer msg=\"not defined yet\""
+	if [ ! -f $DEPLOYMENTCLIENT_CONF ]; then
+		logme "func=setDeploymentServer msg=\"unable to locate $DEPLOYMENTCLIENT_CONF\" status=ko"
+		exit 1
+	fi
+	queryCmd sed
+	SED="sed"
+	$SED -i.orig -e "s/DEFAULTVALUE/$1/g" $DEPLOYMENTCLIENT_CONF	
+	logme "func=setDeploymentServer msg=\"done\" ds=\"$1\" status=ok"
 }
 
 prolog() {
@@ -109,9 +129,18 @@ prolog() {
 	queryCmd cut
 	CUT="cut"
 }
-prolog
-getDeploymentServer #returns DS_OK=1 if DS is foudn
-if [ $DS_OK ]; then
-	setDeploymentServer
+
+#if override file exists, read the content (one line) and use it as splk destination ds
+if [ -s $OVERRIDE_FILE ]; then
+	SPLK_DEST_DS=$(cat $OVERRIDE_FILE)
+else
+	prolog
+	getDeploymentServer
 fi
 
+if [ "$SPLK_DEST_DS" ] ; then
+	setDeploymentServer "$SPLK_DEST_DS"
+	return 0
+fi
+
+return 1
